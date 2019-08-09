85bc5d14b3bbadffd02506d86773ae51ff95d063
ethanASC6 <curlyethan03@gmail.com>
2019-08-09 16:53:57 -0400

    I ADDED THE MAP MARKERS WITH DESCRIPTIONS

--- main.js
+++ main.js
@@ -1,7 +1,43 @@
-var map;
+
 function initMap() {
- map = new google.maps.Map(document.getElementById('map'), {
-   center: {lat: 40.7831, lng: -73.9712},
-   zoom: 8
+  var myLatLng = {lat: -25.363, lng: 131.044};
+
+  var map = new google.maps.Map(document.getElementById('map'), {
+   center: myLatLng,
+   zoom: 4
  });
+
+  var marker = new google.maps.Marker({
+  position: myLatLng,
+  map: map,
+  title: 'wellcome to hellcome'
+});
+var contentString = '<div id="content">'+
+            '<div id="siteNotice">'+
+            '</div>'+
+            '<h1 id="firstHeading" class="firstHeading">Uluru</h1>'+
+            '<div id="bodyContent">'+
+            '<p><b>Uluru</b>, also referred to as <b>Ayers Rock</b>, is a large ' +
+            'sandstone rock formation in the southern part of the '+
+            'Northern Territory, central Australia. It lies 335&#160;km (208&#160;mi) '+
+            'south west of the nearest large town, Alice Springs; 450&#160;km '+
+            '(280&#160;mi) by road. Kata Tjuta and Uluru are the two major '+
+            'features of the Uluru - Kata Tjuta National Park. Uluru is '+
+            'sacred to the Pitjantjatjara and Yankunytjatjara, the '+
+            'Aboriginal people of the area. It has many springs, waterholes, '+
+            'rock caves and ancient paintings. Uluru is listed as a World '+
+            'Heritage Site.</p>'+
+            '<p>Attribution: Uluru, <a href="https://en.wikipedia.org/w/index.php?title=Uluru&oldid=297882194">'+
+            'https://en.wikipedia.org/w/index.php?title=Uluru</a> '+
+            '(last visited June 22, 2009).</p>'+
+            '</div>'+
+            '</div>';
+
+        var infowindow = new google.maps.InfoWindow({
+          content: contentString
+        });
+
+        marker.addListener('click', function() {
+          infowindow.open(map, marker);
+        });
 }
