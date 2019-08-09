5b37443a192a0a4be0ff0880f881b0ec393c66a8
daveASC6 <daveurlajide004@gmail.com>
2019-08-09 18:09:48 -0400

    Added a map

--- main.js
+++ main.js
@@ -1,7 +1,8 @@
 var map;
-function initMap() {
- map = new google.maps.Map(document.getElementById('map'), {
-   center: {lat: 40.7831, lng: -73.9712},
-   zoom: 8
- });
-}
+var mymap = L.map('mapid').setView([51.505, -0.09], 13);
+L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoiZGF2ZWJvbHQxNSIsImEiOiJjano0bTZxZ3EwMjd5M2dxY285YnA5YnM2In0.b67AYjcuzhlLxeUVITvHvg', {
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    id: 'mapbox.streets',
+    accessToken: 'your.mapbox.access.token'
+}).addTo(mymap);
